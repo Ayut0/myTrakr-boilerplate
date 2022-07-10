f2b82179a9509514aed46bf4ab2b915f460859ba
Yuto Yamakita <yuvto.0420@gmail.com>
2022-07-09 21:44:13 -0700

    Add styling and change the way to use tailwind
    npm to CDN

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./frontend/**/*.{html,js}"],
+  content: ["./frontend/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        centered: "0 auto",
+      },
+      width: {
+        30: "30%",
+        90: "90%",
+      },
+    },
   },
   plugins: [],
 };
