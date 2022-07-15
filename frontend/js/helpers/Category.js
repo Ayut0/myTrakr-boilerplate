ad04a1b25ae8200d9287c6f3bd48eb44b246e508
Yuto Yamakita <yuvto.0420@gmail.com>
2022-07-15 16:19:01 -0700

    Refactored category section

--- frontend/js/helpers/Category.js
+++ frontend/js/helpers/Category.js
@@ -1,1 +1,39 @@
+const categoryInput = $("#categoryInput");
+const categorySelect = $("#categorySelect");
+const newCategoryButton = $("#categoryButton");
 
+export const postNewCategory = (categoryInputValue)=>{
+    $.ajax({
+      url: "http://localhost:3000/categories",
+      type: "post",
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        newCategory: {
+          categoryName: categoryInputValue,
+        },
+      }),
+    }).done((data) => {
+      const newCategoryOption = new Option(categoryInputValue);
+      $("#firstOption").remove();
+      categorySelect.prepend(newCategoryOption);
+      categoryInput.hide();
+      newCategoryButton.hide();
+      categoryInput.val("");
+      alert("New category added!!");
+    });
+}
+
+$.ajax({
+  url: "http://localhost:3000/categories",
+  type: "get",
+  contentType: "application/json",
+  dataType: "json",
+}).done((data) => {
+  let categoryOption = $.map(data, (item) => {
+    return `
+           <option value=${item.name.categoryName}>${item.name.categoryName}</option>
+          `;
+  });
+  categorySelect.prepend(categoryOption);
+});
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,9 @@
 import { postNewAccount, getUserData } from "./helpers/Account.js";
+import { postNewCategory } from "./helpers/Category.js";
+
 $(() => {
   //Start coding here!
+  //Create New Account
   let userList = [];
   $("#accountForm").submit((event) => {
     event.preventDefault();
@@ -16,17 +19,13 @@ $(() => {
       alert("This user already exists");
       return;
     }
-
-    //Create New Account
     const newAccount = {
       newAccountName,
       transactions: [],
     };
-
     postNewAccount(newAccount);
     getUserData();
   });
-
 
   //Show and hide select buttons based on transaction type
   const fromSelect = $("#fromButton");
@@ -84,25 +83,25 @@ $(() => {
     event.preventDefault();
     const categoryInputValue = categoryInput.val();
     //Form validation and adding new category
-    categoryInputValue.length > 0 &&
-      $.ajax({
-        url: "http://localhost:3000/categories",
-        type: "post",
-        contentType: "application/json",
-        dataType: "json",
-        data: JSON.stringify({
-          newCategory: {
-            categoryName: categoryInputValue,
-          },
-        }),
-      }).done((data) => {
-        const newCategoryOption = new Option(categoryInputValue);
-        $("#firstOption").remove();
-        categorySelect.prepend(newCategoryOption);
-        categoryInput.hide();
-        newCategoryButton.hide();
-        categoryInput.val("");
-      });
+    categoryInputValue.length > 0 && postNewCategory(categoryInputValue);
+      // $.ajax({
+      //   url: "http://localhost:3000/categories",
+      //   type: "post",
+      //   contentType: "application/json",
+      //   dataType: "json",
+      //   data: JSON.stringify({
+      //     newCategory: {
+      //       categoryName: categoryInputValue,
+      //     },
+      //   }),
+      // }).done((data) => {
+      //   const newCategoryOption = new Option(categoryInputValue);
+      //   $("#firstOption").remove();
+      //   categorySelect.prepend(newCategoryOption);
+      //   categoryInput.hide();
+      //   newCategoryButton.hide();
+      //   categoryInput.val("");
+      // });
   });
 
   let filteredAccount;
